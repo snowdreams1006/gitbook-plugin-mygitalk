b3add2ec8c824baafb9f485e02f701ae911a6f6b
snowdreams1006 <snowdreams1006@163.com>
2019-05-01 17:08:21 +0800

    add index.js

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 module.exports = {
     hooks: {
-	    'page:before': function(page) {
-	      page.content = "# 你好 Title\n" +page.content;
-	      return page;
+    	"init": function() {
+	        console.log("在解析书之后,生成输出页面之前调用: init");
+	    },
+	    "finish：before": function() {
+	        console.log("在生成输出页面后调用,在复制资源,生成封面之前调用: finish：before");
+	    },
+	    "finish": function() {
+	        console.log("所有操作完成后调用: finish");
+	    },
+	    "page:before": function(page) {
+	    	console.log("在页上运行模板引擎之前调用: page:before");
+	      	return page;
 	    }，
 	    "page": function(page) {
-        	page.content = page.content
-        		.replace("<b>", "<strong>")
-	            .replace("</b>", "</strong>");
+	    	console.log("在输出和索引页面之前调用: page");
 	        return page;
 	    }
 	},
