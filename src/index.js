90233942daa9c719962f063cd04dead0a074bcb7
snowdreams1006 <snowdreams1006@163.com>
2019-06-01 21:09:42 +0800

    add params

--- src/index.js
+++ src/index.js
@@ -1,23 +1,33 @@
 import 'gitalk/dist/gitalk.css';
 import Gitalk from 'gitalk';
 
-function component() {
-    var element = document.createElement('div');
+module.exports = {
+    hooks: {
+        'page:before': function(page) {
+            if (this.options.pluginsConfig['mygitalk']) {
+                const clientID = this.options.pluginsConfig['mygitalk']['clientID'];
+                const clientSecret = this.options.pluginsConfig['mygitalk']['clientSecret'];
+                const repo = this.options.pluginsConfig['mygitalk']['repo'];
+                const owner = this.options.pluginsConfig['mygitalk']['owner'];
+                const admin = this.options.pluginsConfig['mygitalk']['admin'];
 
-    element.id = "gitalk-container";
+                const element = document.createElement('div');
+                element.id = "gitalk-container";
+                document.body.appendChild(component());
 
-    return element;
-}
+                const gitalk = new Gitalk({
+                    "clientID": clientID,
+                    "clientSecret": clientSecret,
+                    "repo": repo,
+                    "owner": owner,
+                    "admin": admin,
+                    "id": window.location.pathname,
+                    "distractionFreeMode": false
+                });
+                gitalk.render("gitalk-container");
+            }
 
-document.body.appendChild(component());
-
-const gitalk = new Gitalk({
-    "clientID": "3f62415a283d19cbd696",
-    "clientSecret": "aed0e1db0620bf5d0e3a3f0225f801997ad74e58",
-    "repo": "snowdreams1006.github.io",
-    "owner": "snowdreams1006",
-    "admin": ["snowdreams1006"],
-    "id": window.location.pathname,
-    "distractionFreeMode": false
-});
-gitalk.render("gitalk-container");
+            return page;
+        }
+    }
+};
