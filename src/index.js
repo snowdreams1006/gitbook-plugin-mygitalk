688782d17d41a028dbaaf7c19f27092cafba8a4e
snowdreams1006 <snowdreams1006@163.com>
2019-06-01 21:16:07 +0800

    init

--- src/index.js
+++ src/index.js
@@ -2,32 +2,37 @@ import 'gitalk/dist/gitalk.css';
 import Gitalk from 'gitalk';
 
 module.exports = {
-    hooks: {
-        'page:before': function(page) {
-            if (this.options.pluginsConfig['mygitalk']) {
-                const clientID = this.options.pluginsConfig['mygitalk']['clientID'];
-                const clientSecret = this.options.pluginsConfig['mygitalk']['clientSecret'];
-                const repo = this.options.pluginsConfig['mygitalk']['repo'];
-                const owner = this.options.pluginsConfig['mygitalk']['owner'];
-                const admin = this.options.pluginsConfig['mygitalk']['admin'];
+  hooks: {
+    init() {
+      const site = this.config.get('pluginsConfig.rss');
+      
+      console.log(site);
+    },
+    'page:before': function(page) {
+      if (this.options.pluginsConfig['mygitalk']) {
+        const clientID = this.options.pluginsConfig['mygitalk']['clientID'];
+        const clientSecret = this.options.pluginsConfig['mygitalk']['clientSecret'];
+        const repo = this.options.pluginsConfig['mygitalk']['repo'];
+        const owner = this.options.pluginsConfig['mygitalk']['owner'];
+        const admin = this.options.pluginsConfig['mygitalk']['admin'];
 
-                const element = document.createElement('div');
-                element.id = "gitalk-container";
-                document.body.appendChild(component());
+        const element = document.createElement('div');
+        element.id = "gitalk-container";
+        document.body.appendChild(component());
 
-                const gitalk = new Gitalk({
-                    "clientID": clientID,
-                    "clientSecret": clientSecret,
-                    "repo": repo,
-                    "owner": owner,
-                    "admin": admin,
-                    "id": window.location.pathname,
-                    "distractionFreeMode": false
-                });
-                gitalk.render("gitalk-container");
-            }
+        const gitalk = new Gitalk({
+          "clientID": clientID,
+          "clientSecret": clientSecret,
+          "repo": repo,
+          "owner": owner,
+          "admin": admin,
+          "id": window.location.pathname,
+          "distractionFreeMode": false
+        });
+        gitalk.render("gitalk-container");
+      }
 
-            return page;
-        }
+      return page;
     }
+  }
 };
