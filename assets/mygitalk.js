774ad404a7f3849c59504d2e237c3f594b6c7bda
snowdreams1006 <snowdreams1006@163.com>
2019-06-02 13:31:32 +0800

    add

--- assets/mygitalk.js
+++ assets/mygitalk.js
@@ -1,5 +1,10 @@
 require(["gitbook"], function(gitbook) {
-  gitbook.events.bind('start', function(e, config) {
+  gitbook.events.bind("start", function(e, config) {
+    const gitalk = new Gitalk(config.mygitalk);
+    gitalk.render("gitalk-container");
+  });
+
+  gitbook.events.bind("page.change", function() {
     const gitalk = new Gitalk(config.mygitalk);
     gitalk.render("gitalk-container");
   });
